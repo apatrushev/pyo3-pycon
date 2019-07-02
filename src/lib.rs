1d4a3ffebb02689bab65aadeea75ef53e18d3e13
Anton Patrushev <apatrushev@gmail.com>
2019-07-02 11:12:13 +0100

    initial commit of working project

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+#[pyclass]
+struct MyClass{
+    #[pyo3(get, set)]
+        num: usize,
+        #[pyo3(get, set)]
+        val: String,
+
+}
+
+#[pyfunction]
+fn sum_as_str(a: usize, b: usize) -> MyClass {
+    MyClass {
+        num: (a + b),
+        val: (a+b).to_string()
+    }
+}
+
+#[pymodule]
+fn fizz_buzz(_: Python, m: &PyModule) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(sum_as_str))?;
+    Ok(())
+}
